4784a6bd7d575fb0381397b9f5e45af3c1fba4d5
SoniPrithish <soniprithish25@gmail.com>
2023-05-27 19:19:30 +0000

    sync: 0594 Longest Harmonious Subsequence [Python3]

---
+++ solutions/Arrays/0594-longest-harmonious-subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        freq = {}
+        max_length = 0
+
+        # Count the frequency of each number
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+
+        # Iterate over the numbers and check for harmonious subsequences
+        for num in freq:
+            if num + 1 in freq:
+                length = freq[num] + freq[num + 1]
+                max_length = max(max_length, length)
+
+        return max_length
---
+++ solutions/Arrays/0594-longest-harmonious-subsequence.python3.md
@@ -0,0 +1,8 @@
++++
+problem_title = "Longest Harmonious Subsequence"
+problem_number = "0594"
+difficulty = "Easy"
+topics = ["Array", "Hash Table", "Sliding Window", "Sorting", "Counting"]
+language = "Python3"
+submission_date = "2023-05-27T19:19:30+00:00"
++++
