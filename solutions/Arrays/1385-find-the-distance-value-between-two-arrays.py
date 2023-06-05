9b15230e6c04ffb5513c59a90701521598a41756
SoniPrithish <soniprithish25@gmail.com>
2023-06-05 10:34:22 +0000

    sync: 1385 Find the Distance Value Between Two Arrays [Python3]

--- solutions/Arrays/1385-find-the-distance-value-between-two-arrays.py
+++ solutions/Arrays/1385-find-the-distance-value-between-two-arrays.py
@@ -1,11 +1,11 @@
 class Solution:
     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
-        ct=0
+        ct = 0
+        set_arr2 = set(arr2)
+
         for i in arr1:
-            ct2=0
-            for j in arr2:
-                if abs(i-j)>d:
-                    ct2+=1
-                if ct2==len(arr2):
-                    ct+=1
+            ct2 = sum(abs(i - j) > d for j in set_arr2)
+            if ct2 == len(set_arr2):
+                ct += 1
+
         return ct
--- solutions/Arrays/1385-find-the-distance-value-between-two-arrays.python3.md
+++ solutions/Arrays/1385-find-the-distance-value-between-two-arrays.python3.md
@@ -4,5 +4,5 @@ problem_number = "1385"
 difficulty = "Easy"
 topics = ["Array", "Two Pointers", "Binary Search", "Sorting"]
 language = "Python3"
-submission_date = "2023-06-05T10:30:05+00:00"
+submission_date = "2023-06-05T10:34:22+00:00"
 +++
