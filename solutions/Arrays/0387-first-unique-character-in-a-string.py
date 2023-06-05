5172d2dca0c71290ee6a4371b0eb30a7bd95a907
SoniPrithish <soniprithish25@gmail.com>
2023-06-05 08:38:00 +0000

    sync: 0387 First Unique Character in a String [Python3]

---
+++ solutions/Arrays/0387-first-unique-character-in-a-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        d=dict()
+        ct=-1
+        for i in s:
+            d[i]=d.get(i,0)+1
+        for i in d.keys():
+            if d[i]==1:
+                ct=s.index(i)
+                break
+            
+        return ct
---
+++ solutions/Arrays/0387-first-unique-character-in-a-string.python3.md
@@ -0,0 +1,8 @@
++++
+problem_title = "First Unique Character in a String"
+problem_number = "0387"
+difficulty = "Easy"
+topics = ["Hash Table", "String", "Queue", "Counting"]
+language = "Python3"
+submission_date = "2023-06-05T08:38:00+00:00"
++++
