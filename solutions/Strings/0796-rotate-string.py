cd1bcb22128e3f12be63a55bcbb2eee231712764
SoniPrithish <soniprithish25@gmail.com>
2023-06-08 18:58:41 +0000

    sync: 0796 Rotate String [Python3]

--- solutions/Strings/0796-rotate-string.py
+++ solutions/Strings/0796-rotate-string.py
@@ -2,8 +2,7 @@ class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
         if len(s)!=len(goal):
             return False
-        for i in s:
-            s=s+i
+        s=s+s
         if goal in s:
             return True
         return False
--- solutions/Strings/0796-rotate-string.python3.md
+++ solutions/Strings/0796-rotate-string.python3.md
@@ -4,5 +4,5 @@ problem_number = "0796"
 difficulty = "Easy"
 topics = ["String", "String Matching"]
 language = "Python3"
-submission_date = "2023-06-08T18:56:25+00:00"
+submission_date = "2023-06-08T18:58:41+00:00"
 +++
