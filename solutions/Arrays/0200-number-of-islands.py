a4b33ce8a0c8745e0ccf6fed76e5eed0db692645
SoniPrithish <soniprithish25@gmail.com>
2026-04-13 20:09:15 +0000

    sync: 0200 Number of Islands [Python3]

--- solutions/Arrays/0200-number-of-islands.py
+++ solutions/Arrays/0200-number-of-islands.py
@@ -1,11 +1,12 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
         row=len(grid)
         col=len(grid[0])
         count=0
 
-        if not grid:
-            return 0
+        
         def dfs(r,c):
             if(r<0 or c<0 or r>=row or c>=col or grid[r][c]=='0' ):
                 return
--- solutions/Arrays/0200-number-of-islands.python3.md
+++ solutions/Arrays/0200-number-of-islands.python3.md
@@ -4,5 +4,5 @@ problem_number = "0200"
 difficulty = "Medium"
 topics = ["Array", "Depth-First Search", "Breadth-First Search", "Union-Find", "Matrix"]
 language = "Python3"
-submission_date = "2026-04-13T03:31:59+00:00"
+submission_date = "2026-04-13T20:09:15+00:00"
 +++
